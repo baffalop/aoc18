d594174db944f1515eecece6ce48866dc1830b04
Nikita Gaidakov <ngaidakov@podfather.com>
2020-06-24 23:14:35 +0100

    Tidy and comment FabricClaim

--- src/FabricClaim.hs
+++ src/FabricClaim.hs
@@ -9,6 +9,7 @@ import qualified Data.Set as S
 
 data Rect =
   Rect
+    -- a Set of ids so we can track merged rects after intersect
     { claimId :: S.Set Int
     , left :: Int
     , top :: Int
@@ -27,6 +28,9 @@ bottom rect = top rect + h rect
 intersectAll :: [Rect] -> [Rect]
 intersectAll = foldr addIntersect []
 
+-- recursively intersect a Rect with all Rects in a list
+-- whenever an intersect splits up Rects, we need to individually add each of these
+-- I think this ends up being O(n!)... 🤘
 addIntersect :: Rect -> [Rect] -> [Rect]
 addIntersect r [] = [r]
 addIntersect r (x:xs) =
@@ -34,21 +38,29 @@ addIntersect r (x:xs) =
     Nothing -> x : addIntersect r xs
     Just intersects -> foldr addIntersect xs intersects
 
+-- if two Rects intersect, split them into non-overlapping Rects, one of which will represent the intersection
+-- recursively chop off non-overlapping Rects until we're left with an exact overlap
 intersect :: Rect -> Rect -> Maybe [Rect]
 intersect r1 r2
+  -- Rects overlap exactly: we've found our intersection
   | eqGeometry r1 r2 =
     let mergedIds = S.union (claimId r1) (claimId r2)
      in Just [r1 {claimId = mergedIds, overlap = True}]
+  -- no overlap
   | not horizontalOverlap || not verticalOverlap = Nothing
+  -- chop off left portion
   | not alignedLeft =
     let (leftHalf, rightHalf) = vSplit leftRect $ left rightRect
      in fmap (leftHalf :) $ intersect rightHalf rightRect
+  -- chop off top portion
   | not alignedTop =
     let (topHalf, bottomHalf) = hSplit topRect $ top bottomRect
      in fmap (topHalf :) $ intersect bottomHalf bottomRect
+  -- chop off right portion
   | not widthsCoincide =
     let (leftHalf, rightHalf) = vSplit widerRect $ right narrowerRect
      in fmap (rightHalf :) $ intersect leftHalf narrowerRect
+  -- chop off bottom portion
   | not heightsCoincide =
     let (topHalf, bottomHalf) = hSplit tallerRect $ bottom shorterRect
      in fmap (bottomHalf :) $ intersect topHalf shorterRect
@@ -87,6 +99,6 @@ orderBy f r1 r2
   | otherwise = (r2, r1)
 
 eqGeometry :: Rect -> Rect -> Bool
-eqGeometry r1 r2 =
-  r1 {claimId = S.empty, overlap = False} ==
-  r2 {claimId = S.empty, overlap = False}
+eqGeometry r1 r2 = normalise r1 == normalise r2
+  where
+    normalise r = r {claimId = S.empty, overlap = False}
