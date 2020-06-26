cd4845e618175f34215a73d6ed599156d228f7af
Nikita Gaidakov <ngaidakov@podfather.com>
2020-06-26 01:33:15 +0100

    First definition of CLI using optparse-applicative

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,41 @@
 module Main where
 
+import Data.Semigroup ((<>))
+import Options.Applicative
+
+data Options =
+  Options
+    { day :: Int
+    , part :: DayPart
+    , input :: Maybe String
+    }
+  deriving (Show)
+
+data DayPart
+  = A
+  | B
+  | Both
+  deriving (Show, Read)
+
+cli =
+  let opts =
+        Options <$>
+        (option auto $
+         short 'd' <>
+         long "day" <> metavar "N" <> help "Which day's solution to run") <*>
+        (option auto $
+         short 'p' <>
+         long "part" <>
+         value Both <> help "Which part of the day's solution to run") <*>
+        (optional $
+         strOption $
+         long "input" <>
+         short 'i' <>
+         metavar "FILE" <> help "Override file to use as puzzle's input")
+   in info (opts <**> helper) $
+      fullDesc <>
+      header "Solutions to Advent of Code 2018" <>
+      progDesc "Run solution(s) for the AoC puzzle of the given day"
+
 main :: IO ()
-main = putStrLn "Merry Yuletide!"
+main = print =<< execParser cli
